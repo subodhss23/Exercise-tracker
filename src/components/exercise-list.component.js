17b0c947fe1cf708439c79bc76524d57a502b70d
subodhss23@gmail.com <subodhss23@gmail.com>
2019-07-24 13:00:52 -0700

    adding axios

--- src/components/exercise-list.component.js
+++ src/components/exercise-list.component.js
@@ -1,6 +1,35 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class ExercisesList extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {exercise: []};
+
+    this.deleteExercise = this.deleteExercise.bind(this);
+  }
+
+  componentDidMount(){
+    axios.get('http://localhose:5000/exercises')
+      .then(response => {
+        this.setState ({ exercises: response.data })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  deleteExercise(id){
+    axios.delete('http://localhost:5000/exercises'+id)
+      .then(res => console.log(res.data));
+    this.setState({
+      exercises: this.state.exercises.filter(el => el._id !== id)
+    })
+  }
+
+  
   render() {
     return (
       <div>
